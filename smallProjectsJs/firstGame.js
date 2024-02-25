ecfee0c1f39b42d654cb206f51b45be79101aa80
khalid marzoug <KHALIDMARZOUG9@gmail.com>
2024-02-25 02:12:56 +0100

    first Game finished , still need to print the result

--- smallProjectsJs/firstGame.js
+++ smallProjectsJs/firstGame.js
@@ -21,12 +21,12 @@ function getRandomLeft() {
   return listLeft[randomIndexLeft];
 }
 function getRandomBg() {
-  randomIndexBg = Math.floor(Math.random() * listBg.length);
+  randomIndexBg = Math.floor(Math.random() * listBg.length); // Math.floor return an int ex 5.05 return 5 or 5.95 return 5 or -5.05 return -6
+  // Math.random return a randomly float from 0 to 1
   return listBg[randomIndexBg];
 }
 let checkInterval = setInterval(() => {
   count++;
-  console.log(count);
   if (count === 1) {
     firstBg = getRandomBg();
     firstChild.className = firstChild.className.replace("bg-warning", firstBg);
@@ -38,10 +38,39 @@ let checkInterval = setInterval(() => {
   firstChild.style.left = getRandomLeft();
 }, 2000); // setInerval call a function evry 2s in this case
 
+let listStr;
+let valueStr;
 document.addEventListener("keydown", (event) => {
+  if (secondChild.style.left === "") {
+    listStr = "00%";
+  }
   if (event.key === "ArrowRight") {
-    console.log(event.key);
+    if (listStr === "75%") {
+      return;
+    }
+    valueStr = listStr[0] + listStr[1];
+    valueStr++;
+    valueStr--;
+    valueStr += 25;
+    valueStr += "%";
+    listStr = valueStr;
+    secondChild.style.left = listStr;
   } else if (event.key === "ArrowLeft") {
-    console.log(event.key);
+    if (listStr === "00%") {
+      return;
+    }
+    valueStr = listStr[0] + listStr[1];
+    valueStr++;
+    valueStr--;
+    valueStr -= 25;
+    if (valueStr === 0) {
+      valueStr += "0%";
+      listStr = valueStr;
+      secondChild.style.left = listStr;
+    } else {
+      valueStr += "%";
+      listStr = valueStr;
+      secondChild.style.left = listStr;
+    }
   }
 });
